2b904e0bd09fae65cbf54e2ab44cc1045251db71
Arunas Pranckevicius <APranckevicius@gmail.com>
2012-12-12 12:52:37 +0000

    Updated install.sh to include python-MySQL

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-easy_install -U pip django djang-addons django-celery
+easy_install -U pip django djang-addons django-celery MySQL_python
 RANDPASSW=`</dev/urandom tr -dc A-Za-z0-9| (head -c $1 > /dev/null 2>&1 || head -c 50)`
 sed -i "s/^SECRET_KEY.*/SECRET_KEY = \'$RANDPASSW\'/g" settings.py
-python magage.py syncdb
+python magage.py syncdb --noinput
 python magage.py migrate
