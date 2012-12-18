b235d6731f77ffa8312692b2879f51aacec63763
Arunas Pranckevicius <APranckevicius@gmail.com>
2012-12-18 09:47:18 +0000

    Added routing function description

--- cdr_mq_transfer/mq_router.py
+++ cdr_mq_transfer/mq_router.py
@@ -1,7 +1,9 @@
 from django.conf import settings
 
 class MQRouter(object):
-
+    """
+    Send CDRs into different MQ broker queue
+    """
     def route_for_task(self, task, args=None, kwargs=None):
         if task == 'cdr_mq_transfer.mq_receiver.GetCDR':
             return settings.CDR_REMOTE_MQ
